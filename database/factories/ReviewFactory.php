50793e421480ded30858c19b34602844080cbb35
hafijur <hafijur2584@gmail.com>
2019-07-13 03:20:39 +0600

    initial commit

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model\Review;
+use Faker\Generator as Faker;
+
+$factory->define(Review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
