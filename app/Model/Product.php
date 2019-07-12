dc0b9473bc0257d069d9d1a9139eb6cf5c211e60
hafijur <hafijur2584@gmail.com>
2019-07-13 04:26:16 +0600

    model eloquent complete

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
