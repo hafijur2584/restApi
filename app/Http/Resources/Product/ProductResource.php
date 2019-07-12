bfeb03fadef2263bdf4d517041ad9fe06642ff5c
hafijur <hafijur2584@gmail.com>
2019-07-13 04:55:11 +0600

    product resource created

---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+          'name' => $this->name,
+          'description' => $this->details,
+          'price' => $this->price,
+          'stock' => $this->stock,
+          'discount' => $this->discount
+        ];
+    }
+}
