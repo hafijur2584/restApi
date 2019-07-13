6ee81fb5644ebb385091685daa0444e300a9c3c2
hafijur <hafijur2584@gmail.com>
2019-07-13 06:06:45 +0600

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
           'name' => $this->name,
           'description' => $this->details,
           'price' => $this->price,
-          'stock' => $this->stock,
-          'discount' => $this->discount
+          'total_price' => round((1 - $this->discount/100) *$this->price,2),
+          'stock' => $this->stock == 0 ? 'Out Of Stock' : $this->stock,
+          'discount' => $this->discount,
+          'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2):'No Rating',
+          'href' => [
+              'reviews' => route('reviews.index',$this->id)
+          ]
         ];
     }
 }
